423bbbe6f3c0e9a1c68d359e03dd54c416fbbaf1
Antonio <anto.nunez@hotmail.com>
2018-10-23 14:36:13 +0200

    Ya envía el mensaje del servidor al cliente, me he hecho un print en el servidor para confirmar que recibe el register

--- client.py
+++ client.py
@@ -10,14 +10,17 @@ import sys
 # Constantes. Dirección IP del servidor y contenido a enviar
 SERVER = sys.argv[1]
 PORT = int(sys.argv[2])
-LINE = sys.argv[3]
+MESSTYPE = sys.argv[3]
+USER = sys.argv[4]
+
+MESSAGE = str.upper(MESSTYPE) + ' sip:' + USER + ' SIP/2.0\r\n\r\n'
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((SERVER, PORT))
-    print("Enviando:", LINE)
-    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+    print(MESSAGE)
+    my_socket.send(bytes(MESSAGE, 'utf-8'))
     data = my_socket.recv(1024)
-    print('Recibido -- ', data.decode('utf-8'))
+    print(data.decode('utf-8'))
 
 print("Socket terminado.")
--- server.py
+++ server.py
@@ -7,26 +7,31 @@ import sys
 import socketserver
 
 
-class EchoHandler(socketserver.DatagramRequestHandler):
+class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     """
     Echo server class
     """
-
+    diccionario = {}
     def handle(self):
         """
         handle method of the server class
         (all requests will be handled by this method)
         """
-        self.wfile.write(b"Hemos recibido tu peticion")
+        self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+
         for line in self.rfile:
-            print("El cliente nos manda ", line.decode('utf-8'))
-        print('Desde IP: ' + self.client_address[0] + ', Puerto: ' + str(self.client_address[1]))
+            mess = line.decode('utf-8')
+            if len(mess.split(' ')) != 1:
+                metodo = mess.split(' ')[0]
+                user = mess.split(' ')[1]
+                self.diccionario[user] = self.client_address[0]
+                print(str.upper(metodo) + ' recibido')
 
 if __name__ == "__main__":
     # Listens at localhost ('') port 6001 
     # and calls the EchoHandler class to manage the request
     PORT = int(sys.argv[1])
-    serv = socketserver.UDPServer(('', PORT), EchoHandler) 
+    serv = socketserver.UDPServer(('', PORT), SIPRegisterHandler) 
 
     print("Lanzando servidor UDP de eco...")
     try:
