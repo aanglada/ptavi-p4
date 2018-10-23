0a82aa0d98ed0366b734bb2ad5c42c38bb9c9ac2
Antonio <anto.nunez@hotmail.com>
2018-10-23 15:07:13 +0200

    Ya envía el mensaje del servidor al cliente, me he hecho un print en el servidor para confirmar que recibe el register

--- client.py
+++ client.py
@@ -7,13 +7,17 @@ Programa cliente UDP que abre un socket a un servidor
 import socket
 import sys
 
+if len(sys.argv) != 6:
+    sys.exit('Usage: client.py ip puerto register sip_address expires_value')
+
 # Constantes. Dirección IP del servidor y contenido a enviar
 SERVER = sys.argv[1]
 PORT = int(sys.argv[2])
 MESSTYPE = sys.argv[3]
 USER = sys.argv[4]
+EXPIRES = int(sys.argv[5])
 
-MESSAGE = str.upper(MESSTYPE) + ' sip:' + USER + ' SIP/2.0\r\n\r\n'
+MESSAGE = str.upper(MESSTYPE) + ' sip:' + USER + ' SIP/2.0\r\n' + 'Expires: ' + str(EXPIRES) + '\r\n\r\n'
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
--- server.py
+++ server.py
@@ -6,6 +6,9 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 import sys
 import socketserver
 
+if len(sys.argv) != 2:
+    sys.exit('Usage: python3 server.py port')
+
 
 class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     """
@@ -17,15 +20,24 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
         handle method of the server class
         (all requests will be handled by this method)
         """
-        self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
-
+        mess = []
         for line in self.rfile:
-            mess = line.decode('utf-8')
-            if len(mess.split(' ')) != 1:
-                metodo = mess.split(' ')[0]
-                user = mess.split(' ')[1]
-                self.diccionario[user] = self.client_address[0]
-                print(str.upper(metodo) + ' recibido')
+            mess.append(line.decode('utf-8'))
+        message = mess[0]
+        expires = mess[1].split('\r\n')[0].split(': ')[1]
+        metodo = message.split(' ')[0]
+        user = message.split(' ')[1].split(':')[1]
+        if int(expires) == 0:
+            try:
+                del self.diccionario[user]
+                self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+                print(user + ' eliminado')
+            except KeyError:
+                pass
+        else:
+            self.diccionario[user] = [self.client_address[0], expires]
+            self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+            print(str.upper(metodo) + ' recibido de ' + user)
 
 if __name__ == "__main__":
     # Listens at localhost ('') port 6001 
