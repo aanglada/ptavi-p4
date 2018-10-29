2aa06ce9f131e2da050db16f433636bee16110d6
Antonio <anto.nunez@hotmail.com>
2018-10-29 12:53:54 +0100

    Ya he limpiado el programa conforme a las consideraciones de pep8, solo me faltan 2 lineas que se pasan de 79 caracteres

--- server.py
+++ server.py
@@ -3,7 +3,9 @@
 """
 Clase (y programa principal) para un servidor de eco en UDP simple
 """
-import sys, json, socketserver
+import sys
+import json
+import socketserver
 from datetime import datetime, date, time, timedelta
 
 if len(sys.argv) != 2:
@@ -14,12 +16,14 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     """
     Echo server class
     """
-    diccionario = {}
+    dicc = {}
+
     def handle(self):
         """
         handle method of the server class
         (all requests will be handled by this method)
         """
+        self.json2registered()
         mess = []
         for line in self.rfile:
             mess.append(line.decode('utf-8'))
@@ -29,7 +33,7 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
         user = message.split(' ')[1].split(':')[1]
         if int(expires) == 0:
             try:
-                del self.diccionario[user]
+                del self.dicc[user]
                 self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
                 print(user + ' eliminado')
             except KeyError:
@@ -37,7 +41,7 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
         else:
             address = self.client_address[0] + ':' + str(self.client_address[1])
             expires_time = (datetime.now() + timedelta(seconds=int(expires))).strftime('%H:%M:%S %d-%m-%Y')
-            self.diccionario[user] = {'address' : address, 'expires' : expires_time}
+            self.dicc[user] = {'address': address, 'expires': expires_time}
             self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
             print(str.upper(metodo) + ' recibido de ' + user)
         self.registered2json()
@@ -45,22 +49,28 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     def expires(self):
         now = datetime.now().strftime('%H:%M:%S %d-%m-%Y')
         del_list = []
-        for usuario in self.diccionario:
-            if now >= self.diccionario[usuario]['expires']:
+        for usuario in self.dicc:
+            if now >= self.dicc[usuario]['expires']:
                 del_list.append(usuario)
         for user in del_list:
-            del self.diccionario[user]
-    
+            del self.dicc[user]
+
     def registered2json(self):
-        self.expires()
         with open('registered.json', 'w') as jsonfile:
-            json.dump(self.diccionario, jsonfile, indent=3)
+            json.dump(self.dicc, jsonfile, indent=3)
+
+    def json2registered(self):
+        self.expires()
+        try:
+            with open('registered.json', 'r') as jsonfile:
+                self.dicc = json.load(jsonfile)
+        except:
+            pass
 
 if __name__ == "__main__":
-    # Listens at localhost ('') port 6001 
-    # and calls the EchoHandler class to manage the request
+
     PORT = int(sys.argv[1])
-    serv = socketserver.UDPServer(('', PORT), SIPRegisterHandler) 
+    serv = socketserver.UDPServer(('', PORT), SIPRegisterHandler)
 
     print("Lanzando servidor UDP de eco...")
     try:
